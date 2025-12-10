c20ecdb9a4cface83e25e7e6dcbaead24d2f5b23
Shobhit <shobhitbatra07@gmail.com>
2025-12-10 13:50:15 +0530

    portfolio react template created- commit 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='main'>
+        Hello and Welcome to my portfolio deployed on AWS 
+      </div>
+    </>
+  )
+}
+
+export default App
